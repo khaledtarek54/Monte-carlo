40a47d161374d3d4dc135b23dc7b401a7968463b
Khaled Tarek <52130862+khaledtarek54@users.noreply.github.com>
2020-06-24 13:57:46 +0200

    Add files via upload

---
+++ monte-carlo/monte-carlo/Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace monte_carlo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.demand1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.frequency = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.relativefrequency = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cumulative = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.relativecumulative = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.range = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.week = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.weekdemand = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cost = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.demand1,
+            this.frequency,
+            this.relativefrequency,
+            this.cumulative,
+            this.relativecumulative,
+            this.range,
+            this.week,
+            this.weekdemand,
+            this.cost});
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dataGridView1.Location = new System.Drawing.Point(205, 2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(921, 348);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // demand1
+            // 
+            this.demand1.HeaderText = "demand1";
+            this.demand1.Name = "demand1";
+            this.demand1.ReadOnly = true;
+            // 
+            // frequency
+            // 
+            this.frequency.HeaderText = "frequency";
+            this.frequency.Name = "frequency";
+            this.frequency.ReadOnly = true;
+            // 
+            // relativefrequency
+            // 
+            this.relativefrequency.HeaderText = "relativefrequency";
+            this.relativefrequency.Name = "relativefrequency";
+            this.relativefrequency.ReadOnly = true;
+            // 
+            // cumulative
+            // 
+            this.cumulative.HeaderText = "cumulative";
+            this.cumulative.Name = "cumulative";
+            this.cumulative.ReadOnly = true;
+            // 
+            // relativecumulative
+            // 
+            this.relativecumulative.HeaderText = "relativecumulative";
+            this.relativecumulative.Name = "relativecumulative";
+            this.relativecumulative.ReadOnly = true;
+            // 
+            // range
+            // 
+            this.range.HeaderText = "range";
+            this.range.Name = "range";
+            this.range.ReadOnly = true;
+            // 
+            // week
+            // 
+            this.week.HeaderText = "week";
+            this.week.Name = "week";
+            this.week.ReadOnly = true;
+            // 
+            // weekdemand
+            // 
+            this.weekdemand.HeaderText = "weekdemand";
+            this.weekdemand.Name = "weekdemand";
+            this.weekdemand.ReadOnly = true;
+            // 
+            // cost
+            // 
+            this.cost.HeaderText = "cost";
+            this.cost.Name = "cost";
+            this.cost.ReadOnly = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(113, 21);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(86, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Number of weeks";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Number of runs";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(113, 60);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(86, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "regular cost";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(113, 102);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(86, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 139);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "regular limit";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(113, 136);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(86, 20);
+            this.textBox4.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(10, 171);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Overload cost";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(113, 169);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(86, 20);
+            this.textBox5.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 205);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(187, 62);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Run";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(980, 362);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "regular limit";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn demand1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn frequency;
+        private System.Windows.Forms.DataGridViewTextBoxColumn relativefrequency;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cumulative;
+        private System.Windows.Forms.DataGridViewTextBoxColumn relativecumulative;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn range;
+        private System.Windows.Forms.DataGridViewTextBoxColumn week;
+        private System.Windows.Forms.DataGridViewTextBoxColumn weekdemand;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cost;
+    }
+}
+
---
+++ monte-carlo/monte-carlo/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+namespace monte_carlo
+{
+   
+    public partial class Form1 : Form
+    {
+        List<clist> demand = new List<clist>();
+        List<clist> freqency = new List<clist>();
+        List<clist> relativefreq = new List<clist>();
+        public Form1()
+        {
+
+            InitializeComponent();
+            
+            
+        }
+        int flag = 0;
+        bool stop = false;
+        double tot = 0;
+        double tot2 = 0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Excel.Application xlApp = new Excel.Application();
+            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@"C:\Users\user\Downloads\monte-carlo\monte-carlo\monte-carlo\monte-carlo\bin\Debug\data1.xlsx");
+            Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+            Excel.Range xlRange = xlWorksheet.UsedRange;
+            int k = 0;
+            for (int i = 2; ; i++)
+            {
+                for (int j = 1; j <= 2; j++)
+                {
+
+
+                    //write the value to the console
+                    if (xlRange.Cells[i, j] != null && xlRange.Cells[i, j].Value2 != null)
+                    {
+                        if (j % 2 == 1)
+                        {
+                            clist pnn = new clist();
+                            pnn.val = Convert.ToDouble(xlRange.Cells[i, j].Value2);
+                            tot += pnn.val;
+                            pnn.cumulative = tot;
+                            
+                            demand.Add(pnn);
+                            dataGridView1.Rows.Add();
+                            dataGridView1.Rows[k].Cells[0].Value = pnn.val;
+                            
+                        }
+                        else
+                        {
+                            clist pnn = new clist();
+                            pnn.val = Convert.ToDouble(xlRange.Cells[i, j].Value2);
+                            tot2 += pnn.val;
+                            pnn.cumulative = tot2;
+                            freqency.Add(pnn);
+                            dataGridView1.Rows.Add();
+                            dataGridView1.Rows[k].Cells[1].Value = pnn.val;
+                            dataGridView1.Rows[k].Cells[3].Value = pnn.cumulative;
+                            
+                        }
+                    }
+                    else
+                    {
+                        stop = true;
+                    }
+                    //add useful things here!
+                    
+                }
+                k++;
+                if (stop)
+                {
+                    break;
+                }
+            }
+            tot = 0;
+            for (int i = 0; i < freqency.Count; i++)
+            {
+                clist pnn = new clist();
+                pnn.val = freqency[i].val / freqency[freqency.Count-1].cumulative;
+                tot += pnn.val;
+                pnn.relativecumulative = tot;
+                relativefreq.Add(pnn);
+                dataGridView1.Rows[i].Cells[2].Value = pnn.val;
+                dataGridView1.Rows[i].Cells[4].Value = pnn.relativecumulative;
+            }
+            for (int i = 0; i < freqency.Count; i++)
+            {
+                if (i == 0)
+                {
+                    freqency[i].range = 0;
+                    freqency[i].rangeend = freqency[i].cumulative;
+                    dataGridView1.Rows[i].Cells[5].Value = freqency[i].range +"-"+freqency[i].rangeend;
+                }
+                else
+                {
+                    freqency[i].range = (freqency[i-1].cumulative+1);
+                    freqency[i].rangeend = freqency[i].cumulative;
+                    dataGridView1.Rows[i].Cells[5].Value = freqency[i].range + "-" + freqency[i].rangeend;
+                }
+            }
+            xlWorkbook.Close(true);
+            xlApp.Quit();
+        }
+        public class clist
+        {
+            public double val;
+            public double cumulative;
+            public double relativecumulative;
+            public double range;
+            public double rangeend;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        int rand;
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            Random rr = new Random();
+            for (int i = 1; i <= Convert.ToInt32(textBox1.Text); i++)
+            {
+                double tot = 0;
+                double rest = 0;
+                dataGridView1.Rows[i-1].Cells[6].Value = i;
+                dataGridView1.Rows.Add();
+                rand = rr.Next(0, 100);
+                for (int j = 0; j < demand.Count; j++)
+                {
+                    if (rand >= freqency[j].range && rand <= freqency[j].rangeend)
+                    {
+                        dataGridView1.Rows[i-1].Cells[7].Value = demand[j].val;
+
+                    }
+                }
+
+                if (Convert.ToDouble(dataGridView1.Rows[i-1].Cells[7].Value) < Convert.ToDouble(textBox4.Text))
+                {
+                    dataGridView1.Rows[i-1].Cells[8].Value = Convert.ToDouble(dataGridView1.Rows[i-1].Cells[7].Value) * Convert.ToDouble(textBox3.Text);
+                }
+                else
+                {
+
+                    rest = Convert.ToDouble(dataGridView1.Rows[i-1].Cells[7].Value) - Convert.ToDouble(textBox4.Text);
+                    tot = Convert.ToDouble(textBox4.Text) * Convert.ToDouble(textBox3.Text);
+                    tot += rest * Convert.ToDouble(textBox5.Text);
+                    dataGridView1.Rows[i-1].Cells[8].Value = tot;
+                }
+
+
+            }
+            for (int z = 1; z < Convert.ToInt32(textBox2.Text); z++)
+            {
+                dataGridView1.Columns.Add("weekdemand", "weekdemand");
+                dataGridView1.Columns.Add("cost", "cost");
+                for (int i = 1; i <= Convert.ToInt32(textBox1.Text); i++)
+                {
+
+                    double rest = 0;
+                    rand = rr.Next(0, 100);
+                    for (int j = 0; j < demand.Count; j++)
+                    {
+                        if (rand >= freqency[j].range && rand <= freqency[j].rangeend)
+                        {
+                            dataGridView1.Rows[i-1].Cells[dataGridView1.Columns.Count - 2].Value = demand[j].val;
+                        }
+                    }
+
+
+                    if (Convert.ToDouble(dataGridView1.Rows[i-1].Cells[dataGridView1.Columns.Count - 2].Value) < Convert.ToDouble(textBox4.Text))
+                    {
+                        dataGridView1.Rows[i-1].Cells[dataGridView1.Columns.Count - 1].Value = Convert.ToDouble(dataGridView1.Rows[i-1].Cells[dataGridView1.Columns.Count - 2].Value) * Convert.ToDouble(textBox3.Text);
+                    }
+                    else
+                    {
+
+                        rest = Convert.ToDouble(dataGridView1.Rows[i-1].Cells[dataGridView1.Columns.Count - 2].Value) - Convert.ToDouble(textBox4.Text);
+                        tot = Convert.ToDouble(textBox4.Text) * Convert.ToDouble(textBox3.Text);
+                        tot += rest * Convert.ToDouble(textBox5.Text);
+                        dataGridView1.Rows[i-1].Cells[dataGridView1.Columns.Count - 1].Value = tot;
+                    }
+                }
+            }
+        }
+
+     
+    }
+}
